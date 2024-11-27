ffc6856b59b0c2fdb1d8185c826e63a1da768e18
Jamal <jamal@LAPTOP-HMT2U97A>
2024-11-26 20:41:57 -0500

    Inhale

--- game/p_weapon.c
+++ game/p_weapon.c
@@ -1148,7 +1148,7 @@ void Chaingun_Fire (edict_t *ent)
 		VectorSet(offset, 0, r, u + ent->viewheight-8);
 		P_ProjectSource (ent->client, ent->s.origin, offset, forward, right, start);
 
-		fire_bullet (ent, start, forward, damage, kick, DEFAULT_BULLET_HSPREAD, DEFAULT_BULLET_VSPREAD, MOD_CHAINGUN);
+		Inhaled(ent);
 	}
 
 	// send muzzle flash
@@ -1160,7 +1160,7 @@ void Chaingun_Fire (edict_t *ent)
 	PlayerNoise(ent, start, PNOISE_WEAPON);
 
 	if (! ( (int)dmflags->value & DF_INFINITE_AMMO ) )
-		ent->client->pers.inventory[ent->client->ammo_index] -= shots;
+		ent->client->pers.inventory[ent->client->ammo_index] -= 0;
 }
 
 
